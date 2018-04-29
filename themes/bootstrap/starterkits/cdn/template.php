cf59268b8f90e1c46b83ad4cc9804bfd0c62463f
Radhikasachin <sach.radhika@gmail.com>
2018-04-29 15:50:57 +0200

    second commit

---
+++ themes/bootstrap/starterkits/cdn/template.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @file
+ * The primary PHP file for this theme.
+ */
+
+ /**
+ * Theme the usergreeting block.
+ */
+function THEMENAME_usergreeting_greeting_block(&$variables) {
+  // Return the output of the block here.
+}
